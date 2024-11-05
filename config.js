d2ecf81939b8ab8c43719b8ac8f3f39b3980e10d
Ayanokojix2306 <ayanokojix2306@gmail.com>
2024-11-05 08:18:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 module.exports = {
-    SUDO: process.env.SUDO ? process.env.SUDO.split(',') : ['2348073765008', '2347017895743'], // Fallback to default if not set
+    SUDO: process.env.SUDO ? process.env.SUDO.split(',') : ['2348073765008'], // Fallback to default if not set
     OWNER: process.env.OWNER || 'Kuju', // Updated owner name
     HANDLER: process.env.HANDLER || '.'
+    MODE: process.env.MODE || 'private'
 };
